406f217ab0535cc6916258edbc17fdc6319f8b6c
Vladislav Nikitin <dushekrad@gmail.com>
2020-05-01 17:47:59 +0400

    feat: add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chatify/pages/login.page.dart';
+import 'package:chatify/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  double _deviceHeight;
+  double _deviceWidth;
+
+  GlobalKey<FormState> _formKey;
+
+  _LoginPageState() {
+    _formKey = GlobalKey<FormState>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Align(
+        alignment: Alignment.center,
+        child: _loginPageUI(),
+      ),
+    );
+  }
+
+  Widget _loginPageUI() {
+    return Container(
+      height: _deviceHeight * 0.60,
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.10),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _headingWidget(),
+          _inputForm(),
+          _loginButton(),
+          _registerButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _headingWidget() {
+    return Container(
+      height: _deviceHeight * 0.12,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Text("Welcome back!",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.w700)),
+          Text("Please login to your account.",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w200)),
+        ],
+      ),
+    );
+  }
+
+  Widget _inputForm() {
+    return Container(
+      height: _deviceHeight * 0.16,
+      child: Form(
+        key: _formKey,
+        onChanged: () {},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {},
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+          hintText: "Email Address",
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white))),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      autocorrect: false,
+      obscureText: true,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {},
+      onSaved: (_input) {},
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+          hintText: "Password",
+          focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white))),
+    );
+  }
+
+  Widget _loginButton() {
+    return Container(
+      height: _deviceHeight * 0.06,
+      width: _deviceWidth,
+      child: MaterialButton(
+        onPressed: () {},
+        color: Colors.blue,
+        child: Text(
+          "LOGIN",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+        ),
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return GestureDetector(
+        onTap: () {
+          print("Hey!");
+        },
+        child: Container(
+          height: _deviceHeight * 0.06,
+          width: _deviceWidth,
+          child: Text(
+            "REGISTER",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w700,
+                color: Colors.white60),
+          ),
+        ));
+  }
+}
