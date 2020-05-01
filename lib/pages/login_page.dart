52bd2d7b5aea11f15661227e9f317b7ad8d3a6e3
Vladislav Nikitin <dushekrad@gmail.com>
2020-05-01 18:18:15 +0400

    feat: add auth provider

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import '../providers/auth_provider.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -12,6 +16,9 @@ class _LoginPageState extends State<LoginPage> {
   double _deviceWidth;
 
   GlobalKey<FormState> _formKey;
+
+  String _email;
+  String _password;
 
   _LoginPageState() {
     _formKey = GlobalKey<FormState>();
@@ -69,10 +76,12 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _inputForm() {
     return Container(
-      height: _deviceHeight * 0.16,
+      height: _deviceHeight * 0.18,
       child: Form(
         key: _formKey,
-        onChanged: () {},
+        onChanged: () {
+          _formKey.currentState.save();
+        },
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -90,8 +99,16 @@ class _LoginPageState extends State<LoginPage> {
     return TextFormField(
       autocorrect: false,
       style: TextStyle(color: Colors.white),
-      validator: (_input) {},
-      onSaved: (_input) {},
+      validator: (_input) {
+        return _input.length != 0 && _input.contains("@")
+            ? null
+            : "Please enter a valid email";
+      },
+      onSaved: (_input) {
+        setState(() {
+          _email = _input;
+        });
+      },
       cursorColor: Colors.white,
       decoration: InputDecoration(
           hintText: "Email Address",
@@ -105,13 +122,21 @@ class _LoginPageState extends State<LoginPage> {
       autocorrect: false,
       obscureText: true,
       style: TextStyle(color: Colors.white),
-      validator: (_input) {},
-      onSaved: (_input) {},
+      validator: (_input) {
+        return _input.length != 0 ? null : "Please enter a password";
+      },
+      onSaved: (_input) {
+        setState(() {
+          _password = _input;
+        });
+      },
       cursorColor: Colors.white,
       decoration: InputDecoration(
-          hintText: "Password",
-          focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white))),
+        hintText: "Password",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
     );
   }
 
@@ -120,7 +145,11 @@ class _LoginPageState extends State<LoginPage> {
       height: _deviceHeight * 0.06,
       width: _deviceWidth,
       child: MaterialButton(
-        onPressed: () {},
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            // Login user
+          }
+        },
         color: Colors.blue,
         child: Text(
           "LOGIN",
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+enum AuthStatus {
+  NotAuthenticated,
+  Authenticating,
+  Authenticated,
+  UserNotFound,
+  Error,
+}
+
+class AuthProvider extends ChangeNotifier {
+  FirebaseUser user;
+  AuthStatus status;
+  
+  FirebaseAuth _auth;
+
+  static AuthProvider instance = AuthProvider();
+
+  AuthProvider() {
+    _auth = FirebaseAuth.instance;
+  }
+
+  void loginUserWithEmailAndPassword(String _email, String _password) async {
+    status = AuthStatus.Authenticating;
+
+    notifyListeners();
+    
+    try {
+      AuthResult _result = await _auth.signInWithEmailAndPassword(email: _email, password: _password);
+
+      user = _result.user;
+      status = AuthStatus.Authenticated;
+
+      // Navigate to HomePage
+      print('Logged in successfully');
+    } catch (e) {
+      status = AuthStatus.Error;
+      // Display error
+    }
+
+    notifyListeners();
+  }
+}
